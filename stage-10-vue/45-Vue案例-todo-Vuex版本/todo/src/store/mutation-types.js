8ad602890ad1b09e5ab015f47d2957ba2cd4c04a
FH <315999946@qq.com>
2022-05-03 14:39:07 +0800

    feat: vue

--- stage-10-vue/45-Vue案例-todo-Vuex版本/todo/src/store/mutation-types.js
+++
@@ -1,8 +0,0 @@
-/*
-* 统一actions和mutations中间的所有方法的命名
-* */
-
-export const ADD_TODO = 'add_todo'; //添加一条todo
-export const DEL_TODO = 'del_todo'; //删除一条todo
-export const SELECT_ALL_TODO = 'select_all_todo'; //是否选中所有的todo
-export const DEL_FINISHED_TODO = 'del_finished_todo'; //删除完成的todo'
